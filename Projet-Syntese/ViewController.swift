5d00016af8c617634bdd520c769ba364b52e6ad7
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-12-27 17:45:30 +0100

    Initial Commit

---
+++ Projet-Syntese/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projet-Syntese
+//
+//  Created by M'haimdat omar on 27-12-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
